10a3977b3553405653485f3d28207eb9bb8cbc3e
Barbara <bacs96@gmail.com>
2017-07-10 14:51:46 -0300

    Matheus bugou algo

---
+++ T2_PO2/Métodos/CoordCiclicas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_PO2.Métodos
+{
+    public class CoordCiclicas
+    {
+
+    }
+}
