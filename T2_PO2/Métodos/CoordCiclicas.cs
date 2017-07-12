4a17b9c0441c8a2460b648c86c5d7e8ae5438160
Barbara <bacs96@gmail.com>
2017-07-12 20:16:03 -0300

    começo das ciclicas

--- T2_PO2/Métodos/CoordCiclicas.cs
+++ T2_PO2/Métodos/CoordCiclicas.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using info.lundin.math;
+using T1_PO2.Métodos;
 
 namespace T2_PO2.Métodos
 {
     public class CoordCiclicas
     {
-        public void Calcular(string func, double[] x1, double erro)
+        Newton minimiza = new Newton();
+        public void Calcular(string func, double[] xk, double erro, int n)
         {
-
+            minimiza.infos.func = func;
+            minimiza.infos.a = xk[n-1];
+            minimiza.infos.e = erro;
+            int k = 0;
+            double[] d, xk, xk1;
+            string yj, yj1, funcyj;
+            do
+            {
+                k++;
+                yj = xk;
+                /*Monta o vetor d*/
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        d[j] = 0;
+                    }
+                    d[i] = 1;
+                }
+                /**/
+                yj = fazyj();
+                lamb = Newton(gerafunc(yj), xk[n-j], erro);
+                yj = calcyj(lamb);
+                xk1 = yj1;
+            } while ((xk1 - xk) < erro);
         }
     }
 }
